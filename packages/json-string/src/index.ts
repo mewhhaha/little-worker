331784b3afd4e22cee4b9bcd041864ce0451c109
Jacob Torrång <kopatheonlyone@hotmail.com>
2023-07-15 15:35:08 +0200

    Serialize in json-string

--- packages/json-string/src/index.ts
+++ packages/json-string/src/index.ts
@@ -13,4 +13,26 @@ export type JSONString<T> = string & {
   readonly __value: T;
 };
 
-export type JSONOf<T> = T extends JSONString<infer JSON> ? JSON : never;
+export type JSONOf<T> = T extends JSONString<infer JSON>
+  ? SerializedObject<JSON>
+  : never;
+
+type Primitive = string | number | boolean | null | undefined;
+
+type SerializedJSON<T> = T extends Primitive
+  ? T
+  : T extends Array<infer U>
+  ? SerializedArray<U>
+  : T extends Date
+  ? string
+  : T extends (...args: any[]) => any
+  ? undefined
+  : T extends object
+  ? SerializedObject<T>
+  : undefined;
+
+type SerializedArray<T> = Array<SerializedJSON<T>>;
+
+type SerializedObject<T> = {
+  [P in keyof T]: T[P] extends Primitive ? T[P] : SerializedJSON<T[P]>;
+};
